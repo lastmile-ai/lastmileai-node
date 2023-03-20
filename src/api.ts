e5cfae05208c0d64391137d0783b83e65cffde88
Ryan Holinshead <5060851+rholinshead@users.noreply.github.com>
2023-03-19 21:16:36 -0400

    More types

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,101 @@
 import axios from "axios";
-import { JSONObject } from "./common";
+import { JSONValue } from "./common";
 import { Configuration } from "./configuration";
 
+/**
+ * GENERAL TYPES
+ */
 
 /**
- * EMBEDDINGS
+ * 
+ * @export
+ * @interface Upload
+ */
+export interface Upload {
+    /**
+     * 
+     * @type {string}
+     * @memberof Upload
+     */
+    'id': string;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Upload
+     */
+    'createdAt': Date;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Upload
+     */
+    'updatedAt': Date;
+    /**
+     * 
+     * @type {string}
+     * @memberof Upload
+     */
+    'url': string;
+    /**
+     * 
+     * @type {JSONValue}
+     * @memberof Upload
+     */
+    'metadata': JSONValue;
+    /**
+     * 
+     * @type {string}
+     * @memberof Upload
+     */
+    'modelId': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof Upload
+     */
+    'creatorId': string;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof Upload
+     */
+    'active': boolean;
+    /**
+     * 
+     * @type {JSONValue | null}
+     * @memberof Upload
+     */
+    'attributes': JSONValue | null;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Upload
+     */
+    'trialStepId': string | null;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Upload
+     */
+    'embeddingDataId': string | null;
+}
+
+
+export enum Visibility {
+    // Only visible to creator
+    PRIVATE = 'PRIVATE',
+    // Only visible to creator and recursive members of entity and parent entities.
+    // For example, if you create trial T in experiment E in workspace W,
+    // T will be visible to members explicitly specified for T, plus
+    // members with access to E, plus
+    // members with access to W.
+    MEMBER = 'MEMBER',
+    // Visible to everyone
+    PUBLIC = 'PUBLIC',
+}
+
+/**
+ * EMBEDDING TYPES
  */
 
 /**
@@ -30,13 +121,13 @@ export interface EmbeddingCollection {
      * @type {Date}
      * @memberof EmbeddingCollection
      */
-    'updatedAt': number;
+    'updatedAt': Date;
     /**
      * 
-     * @type {JSONObject}
+     * @type {JSONValue}
      * @memberof EmbeddingCollection
      */
-    'metadata': JSONObject;
+    'metadata': JSONValue;
     /**
      * 
      * @type {string}
@@ -67,6 +158,80 @@ export interface EmbeddingCollection {
      * @memberof EmbeddingCollection
      */
     'promoted': boolean;
+    /**
+     * 
+     * @type {string}
+     * @memberof EmbeddingCollection
+     */
+    'creatorId': string;
+    /**
+     * 
+     * @type {?string}
+     * @memberof EmbeddingCollection
+     */
+    'organizationId': string | null;
+    /**
+     * 
+     * @type {Visibility}
+     * @memberof EmbeddingCollection
+     */
+    'visibility': Visibility;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof EmbeddingCollection
+     */
+    'active': boolean;
+    /**
+     * 
+     * @type {Array<EmbeddingData>}
+     * @memberof EmbeddingCollection
+     */
+    'embeddings': Array<EmbeddingData>;
+    /**
+     * 
+     * @type {Array<Model>}
+     * @memberof EmbeddingCollection
+     */
+    'models': Array<Model>;
+}
+
+/**
+ * 
+ * @export
+ * @interface EmbeddingData
+ */
+export interface EmbeddingData {
+    /**
+     * 
+     * @type {string}
+     * @memberof EmbeddingData
+     */
+    'id': string;
+    /**
+     * 
+     * @type {Date}
+     * @memberof EmbeddingData
+     */
+    'createdAt': Date;
+    /**
+     * 
+     * @type {Date}
+     * @memberof EmbeddingData
+     */
+    'updatedAt': Date;
+    /**
+     * 
+     * @type {string}
+     * @memberof EmbeddingData
+     */
+    'uploadId': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof EmbeddingData
+     */
+    'embeddingCollectionId': string;
 }
 
 /**
@@ -152,6 +317,262 @@ export type ReadEmbeddingCollectionResponse = EmbeddingCollection;
 export type UpdateEmbeddingCollectionResponse = EmbeddingCollection;
 
 /**
+ * EXPERIMENT TYPES
+ */
+
+/**
+ * 
+ * @export
+ * @interface Experiment
+ */
+export interface Experiment {
+    /**
+     * 
+     * @type {string}
+     * @memberof Experiment
+     */
+    'id': string;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Experiment
+     */
+    'createdAt': Date;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Experiment
+     */
+    'updatedAt': Date;
+    /**
+     * 
+     * @type {string}
+     * @memberof Experiment
+     */
+    'creatorId': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof Experiment
+     */
+    'workspaceId': string;
+    /**
+     * 
+     * @type {Visibility}
+     * @memberof Experiment
+     */
+    'visibility': Visibility;
+    /**
+     * 
+     * @type {string}
+     * @memberof Experiment
+     */
+    'name': string;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Experiment
+     */
+    'description': string | null;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Experiment
+     */
+    'modelId': string | null;
+    /**
+     *
+     * @type {JSONValue | null}
+     * @memberof Experiment
+     */
+    'parameters': JSONValue | null;
+    /**
+     * The input-output schema for inference for this experiment
+     * @type {JSONValue | null}
+     * @memberof Experiment
+     */
+    'inferenceSchema': JSONValue | null;
+    /**
+     * The input-output schema for fine-tuning for this experiment
+     * @type {JSONValue | null}
+     * @memberof Experiment
+     */
+    'fineTuningSchema': JSONValue | null;
+    /**
+     *
+     * @type {Array<Trial>}
+     * @memberof Experiment
+     */
+    'trials': Array<Trial>;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Experiment
+     */
+    'primaryTrialId': string | null;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof Experiment
+     */
+    'active': boolean;
+    /**
+     *
+     * @type {JSONValue | null}
+     * @memberof Experiment
+     */
+    'attributes': JSONValue | null;
+}
+
+/**
+ * MODEL TYPES
+ */
+
+/**
+ * 
+ * @export
+ * @interface Model
+ */
+export interface Model {
+    /**
+     * 
+     * @type {string}
+     * @memberof Model
+     */
+    'id': string;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Model
+     */
+    'createdAt': Date;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Model
+     */
+    'updatedAt': Date;
+    /**
+     * 
+     * @type {string}
+     * @memberof Model
+     */
+    'creatorId': string;
+    /**
+     * 
+     * @type {string}
+     * @memberof Model
+     */
+    'organizationId': string;
+    /**
+     * 
+     * @type {Visibility}
+     * @memberof Model
+     */
+    'visibility': Visibility;
+    /**
+     * 
+     * @type {string}
+     * @memberof Model
+     */
+    'name': string;
+    /**
+     * 
+     * @type {?string}
+     * @memberof Model
+     */
+    'description': string | null;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof Model
+     */
+    'promoted': boolean;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof Model
+     */
+    'active': boolean;
+    /**
+     * The input-output schema for inference for this model
+     * @type {JSONValue | null}
+     * @memberof Model
+     */
+    'inferenceSchema': JSONValue | null;
+    /**
+     * The input-output schema for fine-tuning for this model
+     * @type {JSONValue | null}
+     * @memberof Model
+     */
+    'fineTuningSchema': JSONValue | null;
+    /**
+     *
+     * @type {?string}
+     * @memberof Model
+     */
+    'inferenceEndpoint': string | null;
+    /**
+     *
+     * @type {Array<Upload>}
+     * @memberof Model
+     */
+    'modelUploads': Array<Upload>;
+    /**
+     *
+     * @type {JSONValue | null}
+     * @memberof Model
+     */
+    'attributes': JSONValue | null;
+    /**
+     *
+     * @type {Array<Experiment>}
+     * @memberof Model
+     */
+    'Experiment': Array<Experiment>;
+}
+
+/**
+ * TRIAL TYPES
+ */
+
+/**
+ * 
+ * @export
+ * @interface Trial
+ */
+export interface Trial {
+    /**
+     * 
+     * @type {string}
+     * @memberof Trial
+     */
+    'id': string;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Trial
+     */
+    'createdAt': Date;
+    /**
+     * 
+     * @type {Date}
+     * @memberof Trial
+     */
+    'updatedAt': Date;
+    /**
+     * 
+     * @type {string}
+     * @memberof Trial
+     */
+    'creatorId': string;
+}
+
+/**
+ * API Class
+ */
+
+/**
  * LastMileAIApi - API for interfacing with LastMileAI
  * @export
  * @class LastMileAIApi
@@ -203,7 +624,7 @@ export class LastMileAIApi {
 
     /**
     * 
-    * @summary Returns a list of embedding collections. Supports pagination and filtering by name
+    * @summary Returns a list of EmbeddingCollections. Supports pagination and filtering by name
     * @param {EmbeddingCollectionsListQueryData} [queryData] The cursor id of the last item provided by listEmbeddingCollections API
     */
     public async listEmbeddingCollections(queryData?: EmbeddingCollectionsListQueryData): Promise<ListEmbeddingCollectionsResponse> {
@@ -220,8 +641,8 @@ export class LastMileAIApi {
 
     /**
     * 
-    * @summary Reads an embedding collection
-    * @param {string} id The id of the embedding collection to read
+    * @summary Reads an EmbeddingCollection
+    * @param {string} id The id of the EmbeddingCollection to read
     */
     public async readEmbeddingCollection(id: string): Promise<ReadEmbeddingCollectionResponse> {
         const res = await axios.get('embeddings/read', { ...this.configuration.defaultAxiosConfig, params: { id } });
@@ -230,8 +651,8 @@ export class LastMileAIApi {
 
     /**
     * 
-    * @summary Reads an embedding collection
-    * @param {string} id The id of the embedding collection to read
+    * @summary Update the data associated with an EmbeddingCollection
+    * @param {string} id The id of the EmbeddingCollection to update
     * @param {EmbeddingCollectionUpdateData} data Data to update for the EmbeddingCollection
     */
     public async updateEmbeddingCollection(id: string, data: EmbeddingCollectionUpdateData): Promise<UpdateEmbeddingCollectionResponse> {
